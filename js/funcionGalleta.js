4864502238128002b41c2ec023743aa291d62f01
Dani <dpajaresfo@gmail.com>
2025-05-12 19:04:25 +0200

    añadimos la funcion de mostrarPropiedades

--- js/funcionGalleta.js
+++ js/funcionGalleta.js
@@ -42,3 +42,14 @@ function mostrarValores(){
     resultado.textContent= propiedades;
     productoDiv.appendChild(resultado);
 }
+function mostrarPropiedades(){
+    let propiedades = "";
+    for (let key in galletas) {
+        propiedades += key + ", ";
+    }
+    // Quitar la última coma y espacio
+    propiedades = propiedades.slice(0, -2);
+    const resultado = document.createElement("p");
+    resultado.textContent= propiedades;
+    productoDiv.appendChild(resultado);
+}
